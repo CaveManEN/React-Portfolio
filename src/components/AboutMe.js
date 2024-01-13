f25c37eb78da95f97958631d52da758f79b9aa9f
CaveManEN <ianccody@gmail.com>
2024-01-12 21:40:55 -0600

    minor fixes to code to help deploy

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,7 @@ const AboutMe = () => {
     <div>
       <h2>About Me</h2>
       {/* Add your photo/avatar and bio here */}
-      <img src="%PUBLIC_URL%/assets/images/DANTE-HEADSHOT-GLASSES.jpg" alt="Dante's Headshot" />
+      <img src="%PUBLIC_URL%/assets/images/ianportrait.PNG" alt="ians portrait" />
       <p>Your bio goes here...</p>
     </div>
   );
